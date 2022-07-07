16b64cc7a997d78fab94c6c1057a20d6b0df2b67
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-07-07 17:41:39 +0530

    initial settup

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dev C Krishna on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
